9f6eecbe8ac1c1a41d5b9ed7d073de41144110d4
Kohei107 <k.s107bb@gmail.com>
2020-10-08 11:54:13 +0900

    Top画面実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'tasks/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'tasks#index'
+  root to: 'top#index'
   resources :tasks, only: [:index, :new]
 end
