e5f8150e160651d78b80feaeb576465aa9d1112f
Kohei107 <k.s107bb@gmail.com>
2020-10-09 15:21:15 +0900

    追加フォーム実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   #get 'tasks/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'top#index'
-  resources :tasks, only: [:index, :new]
+  resources :tasks, only: [:index]
+  resources :lists, only: [:new, :create]
 end
