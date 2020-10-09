de405585faf7906f8a6b8c7ab725cf7238bc08cb
Kohei107 <k.s107bb@gmail.com>
2020-10-09 18:20:17 +0900

    リスト作成してtasks#indexに遷移

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,15 +1,22 @@
 class ListsController < ApplicationController
 
   def new
-    @lists =List.new
+    @list = List.new
   end
 
   def create
+    
     @list = List.new(list_params)
-    if @list.save
+    if @list.save!
       redirect_to tasks_path
     else
       render :new
     end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name).merge(user_id: current_user.id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   #get 'tasks/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'top#index'
-  resources :tasks, only: [:index]
-  resources :lists, only: [:new, :create]
+
+  resources :lists, only: [:new, :create, :destroy] 
+   resources :tasks, only: [:index, :create, :destroy]
+  
+
 end
