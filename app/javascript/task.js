0fdd48b5c4ac03b0953d0abe633ee8d4332c7759
Kohei107 <k.s107bb@gmail.com>
2020-10-14 21:00:33 +0900

    viewの調整

---
+++ app/javascript/task.js
@@ -0,0 +1,42 @@
+// function task() {
+//   const submit = document.getElementById("submit")
+//   submit.addEventListener("click", (e) => {
+//     const formData = new FormData(document.getElementById("form"));
+//     const XHR = new XMLHttpRequest();
+//     XHR.open("POST", `/lists/:${gon.list}/tasks${(gon.list)}` , true)
+//     XHR.responseType = "json";
+//     XHR.send(formData);
+//     XHR.onload = () => {
+//       if (XHR.status != 200) {
+//         alert('Error ${XHR.status}: ${XHR.statusText}');
+//         return null;
+//       }
+//       const item = XHR.response.post;
+//       const list = document.getElementById("list");
+//       const formText = document.getElementById("content");
+//       const HTML = `
+//       <div class="task">
+//     <div class="task-content">
+//     <%= check_box_tag :done %>
+    
+//       <%= task.content %>
+//     </div>
+//   <div class="map">
+//     <iframe 
+//         width="500"
+//         height="100"
+//         frameborder="0" style="border:0"
+//         src=<%=url_for("https://www.google.com/maps/embed/v1/place?key=AIzaSyAPg4reLN0KHo2VmTyrMs5v75h6UNk6RK4&q=#{task.content}&zoom=15")%>>
+//     </iframe>
+//   </div>
+//       <div class delete-btn>
+//         <%= link_to "削除", list_task_path(@list.id, task.id), method: :delete %>
+//       </div>
+//   </div>`;
+//   list.insertAdjacentHTML("afterend", HTML);
+//      formText.value = "";
+//     };
+//     e.preventDefault();
+//   });
+// }
+// window.addEventListener("load", task);
