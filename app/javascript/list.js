e1a8d065d4a3ef7fd218519e37acca8049e84fd6
Kohei107 <k.s107bb@gmail.com>
2020-10-15 18:57:24 +0900

    洗濯しているリストを表示

---
+++ app/javascript/list.js
@@ -0,0 +1,7 @@
+// window.addEventListener("load",function(){
+//   document.getElementById('list-name').addEventListener('click',function(){
+//     const listName = document.getElementById('list-name');
+//     listName.setAttribute("style","background:pink;");
+// })
+
+// });
