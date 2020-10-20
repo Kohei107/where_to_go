c2a058ac8fe1ce9bb8759b88597890fc5d54a84a
Kohei107 <k.s107bb@gmail.com>
2020-10-20 18:42:29 +0900

    viewの調整

--- app/javascript/list.js
+++ app/javascript/list.js
@@ -1,5 +1,5 @@
  window.addEventListener("load",function(){
-  const listKey = document.getElementById("list_path");
+  //const listKey = document.getElementById("list_path");
   const listColor = location.pathname
   $('a[href="'+listColor+'"]').css({"background-Color":"yellow"});
 });
